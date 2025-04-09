2f158fc3f87c8b5f2e3b8e1c178c5492268b0ba5
sagar460 <shresthasagr7000@gmail.com>
2025-04-09 16:45:12 +0545

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/HOme";
+import Home from "./pages/Home";
+import SingleBlog from "./pages/SingleBlog";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/blog/:id" element={<SingleBlog />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,25 @@
+import { useEffect, useState } from "react";
 import Card from "../assets/components/Card";
 import Navbar from "../assets/components/Navbar";
+import axios from "axios";
 
 function Home() {
+  const [blogs, setBlogs] = useState([]);
+  const fetchBlogs = async () => {
+    const response = await axios.get("http://localhost:3000/blog/");
+    setBlogs(response.data.data);
+  };
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+  console.log(blogs);
   return (
     <>
       <Navbar />
       <div className="flex flex-wrap">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {blogs.map(function (blog) {
+          return <Card key={blog._id} blog={blog} />;
+        })}
       </div>
     </>
   );
