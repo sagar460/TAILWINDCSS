0d722d5dc5130e371eaf9e1d59e51b8c9c9ac80e
sagar460 <shresthasagr7000@gmail.com>
2025-04-20 16:07:45 +0545

    new commit

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,6 +1,32 @@
+import axios from "axios";
 import Navbar from "../assets/components/Navbar";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 function SingleBlog() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [blog, setBlog] = useState({});
+  // console.log(id);
+  const fetchSingleBlog = async () => {
+    const response = await axios.get("http://localhost:3000/blog/" + id);
+    setBlog(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchSingleBlog();
+  }, []);
+
+  const deleteFunction = async () => {
+    const response = await axios.delete("http://localhost:3000/blog/" + id);
+    console.log(response.status);
+    if (response.status === 200) {
+      alert("Deleted Successfully");
+      navigate("/");
+    } else {
+      alert("Something went wrong");
+    }
+  };
   return (
     <>
       <Navbar />
@@ -11,7 +37,7 @@ function SingleBlog() {
               <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
                 <img
                   className="w-full h-full object-cover"
-                  src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg"
+                  src={`http://localhost:3000/${blog.image}`}
                   alt="Product Image"
                 />
               </div>
@@ -22,7 +48,10 @@ function SingleBlog() {
                   </button>
                 </div>
                 <div className="w-1/2 px-2">
-                  <button className="w-full bg-gray-200 dark:bg-red-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">
+                  <button
+                    className="w-full bg-gray-200 dark:bg-red-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600"
+                    onClick={deleteFunction}
+                  >
                     DELETE ME
                   </button>
                 </div>
@@ -30,26 +59,14 @@ function SingleBlog() {
             </div>
             <div className="md:flex-1 px-4">
               <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">
-                Product Name
+                {blog.title}
               </h2>
-              <p className="text-gray-600 dark:text-gray-300 text-sm mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-                ante justo. Integer euismod libero id mauris malesuada
-                tincidunt.
-              </p>
               <div>
                 <span className="font-bold text-gray-700 dark:text-gray-300">
-                  Product Description:
+                  {blog.subtitle}
                 </span>
                 <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  sed ante justo. Integer euismod libero id mauris malesuada
-                  tincidunt. Vivamus commodo nulla ut lorem rhoncus aliquet.
-                  Duis dapibus augue vel ipsum pretium, et venenatis sem
-                  blandit. Quisque ut erat vitae nisi ultrices placerat non eget
-                  velit. Integer ornare mi sed ipsum lacinia, non sagittis
-                  mauris blandit. Morbi fermentum libero vel nisl suscipit, nec
-                  tincidunt mi consectetur.
+                  {blog.description}
                 </p>
               </div>
             </div>
